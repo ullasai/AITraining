47b8fb1caf12075a6ad4f951476b91ec838d1539
Ullas Lakshminarayana <ullaslakshminarayana@MacBook-Pro.local>
2026-04-12 22:29:48 -0500

    add basic python code

---
+++ pythoncode/inputoutput.py
@@ -0,0 +1,6 @@
+# in-built function print () and input(), range(), len(), type(), int(), float(), str(), list(), dict(), set(), tuple() etc
+print("I am learning Python")
+a=input("What is your name?: ")
+print("My name is :", a)
+
+
